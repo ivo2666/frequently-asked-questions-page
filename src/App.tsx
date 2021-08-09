03d50f679ec1c9d8759d901412851e8dc2db3c09
ivo2666 <ivailocvetkov365@gmail.com>
2021-08-09 18:02:56 +0300

    add: App, Question and Title components

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,43 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Title } from './components';
+import { Question } from './components';
+import styled from 'styled-components';
+
+const StyledApp = styled.div`
+margin:auto;
+width: 50%;
+`
 
 function App() {
+  const [data, setData] = React.useState(
+    [
+      { question: 'question 1', answer: 'answer 1', answerIsOpen: false },
+      { question: 'question 2', answer: 'answer 2', answerIsOpen: false },
+      { question: 'question 3', answer: 'answer 3', answerIsOpen: false },
+      { question: 'question 4', answer: 'answer 4', answerIsOpen: false },
+    ]
+  );
+
+  const handleClick = (id: number) => {
+    let value = data[id].answerIsOpen;
+    let newData: any = [...data];
+    newData.map((element: any) => {
+      return element.answerIsOpen = false;
+    });
+      newData[id].answerIsOpen = !value;
+    setData(newData);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <StyledApp>
+      <Title />
+      {data.map((el, i) => {
+        return (
+          <Question key={i} data={el} id={i} onClick={handleClick} />
+        )
+      })}
+    </StyledApp>
   );
 }
 
---
+++ src/components/index.js
@@ -0,0 +1,7 @@
+import Question from './Question'
+import Title from './Title'
+
+export {
+    Question,
+    Title
+}
